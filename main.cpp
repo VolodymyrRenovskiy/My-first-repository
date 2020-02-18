b504669d62aa30759271c6c7ba378c7eb6a655de
Volodymyr <>
2020-02-18 23:50:59 +0200

    Commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,43 @@
 
-another code...
+#include <iostream>
+
+#include <bitset>
+
+int main(int argc, char const *argv[])
+{
+    //std::cout << "Some text 2";
+
+    bool state = true;
+
+    std::cout << state;
+
+/*
+    int value {10};
+    int value2 {20};
+
+    std::cout << std::boolalpha;
+    bool is_state = (value  == value2);
+    std::cout << is_state << std::endl;
+
+    std::cout << std::boolalpha;
+    is_state = (value  != value2);
+    std::cout << is_state << std::endl;
+
+    int a = 41;
+    bool is_access = a > 20 && a <= 40;
+    std::cout << is_access << std::endl;
+*/
+
+/*
+    std::bitset<8> bit(126);
+
+    std::cout << bit.to_string('X') << std::endl;
+
+    bit.set(2, false);
+
+    std::cout << bit.to_string('X') << std::endl;
+
+    std::cout << bit.count() << std::endl;
+*/
+    return 0;
+}
